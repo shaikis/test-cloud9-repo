8d460518054f7cb500eb998c6c0aa52e5a7568d3
Ismails <shaik.urs@gmail.com>
2022-02-26 15:51:52 +0530

    added deployment repo

--- main.tf
+++ main.tf
@@ -1,2 +1,21 @@
-###
-#
+# Declare backend.
+provider "aws" {
+  region  = "${var.region}"
+  profile = "${var.aws_profile}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "ismail-tio-test"
+    key    = "vpc/cloud9/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+module "cloud9" {
+    source = "https://github.com/shaikis/terraform-aws-cloud9.git"
+    cloud9_env_name = var.cloud9_environment
+    region = var.region
+    cloud9_instance_type = var.cloud9_instance_type
+    automatic_stop_time_minutes = var.cloud9_automatic_stop_time
+}
---
+++ variabled.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    default = eu-west-1
+}
+variable "aws_profile" {
+    default = "cloud9"
+    description = "configure AWS CLI profile"
+}
+variable "cloud9_environment" {
+    default = test
+}
+variable "cloud9_instance_type" {
+    default = t2.micro
+}
+variable "cloud9_automatic_stop_time" {
+    default = 30
+}
